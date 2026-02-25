48af8451cf8b8c0143d289ecc5b2d196cd6395d9
Akiyakayne <antoniy35@gmail.com>
2026-02-25 14:12:16 +0300

    Обновление турнирной таблицы

---
+++ config.example.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    SPREADSHEET_ID: '',
+    API_KEY: '',
+    SHEETS: {
+        MAIN: 'ОСНОВНАЯ ТАБЛИЦА',
+        TEAMS: 'ТАБЛИЦА ПО КОМАНДАМ',
+        PLAYERS: 'ТАБЛИЦА ПО ИГРОКАМ'
+    }
+};
