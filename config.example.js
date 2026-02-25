4b83155a61a13541562c93f7548b1dc4de1de1e3
Akiyakayne <antoniy35@gmail.com>
2026-02-25 14:38:22 +0300

    v1

--- config.example.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-    SPREADSHEET_ID: '',
-    API_KEY: '',
-    SHEETS: {
-        MAIN: 'ОСНОВНАЯ ТАБЛИЦА',
-        TEAMS: 'ТАБЛИЦА ПО КОМАНДАМ',
-        PLAYERS: 'ТАБЛИЦА ПО ИГРОКАМ'
-    }
-};
